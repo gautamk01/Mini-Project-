bb04cc70dea96b34e04b2d43364c295d6f50dcc5
gautamk01 <charmingdeveloper@protonmail.com>
2023-12-06 12:04:25 +0530

    Added the Help

--- uifinal.py
+++ uifinal.py
@@ -58,7 +58,8 @@ def create_dropdowns():
         for a in range(0, len(disease)):
             if (result == a):
                 final = disease[a]
-        st.success(f'{final.replace("_", " ")} {" Disease"}')
+        st.success(
+            f'{"Your cow is likely to have a "}{final.replace("_", " ")}')
 
 
 def load_lottiefile(filepath: str):
@@ -117,6 +118,105 @@ def main():
     with col2:
         st.image(gif, use_column_width=True)
 
+    expander = st.expander("Help Desk")
+    expander.write(""" 
+    # Help Desk
+                   
+    ## Body temperature of cow
+
+        A thermometer is an extremely useful and inexpensive piece of equipment for cattle keepers. 
+        
+        how to measure it 📏 
+        The cow should be adequately restrained using a crush, the thermometer inserted into the rectum and pressed up against the mucosa (lining) of the rectum. The normal temperature of an adult cow is around 38.5°C.
+
+    ## Body Condition Score
+
+    Body condition refers to the relative amount of subcutaneous body fat or energy reserve in the cow.A 5-point (1-5) scoring system to measure the relative amount of this subcutaneous body fat. 
+    ### how to measure it
+    #### Resource 
+    [document](https://www.vet.cornell.edu/sites/default/files/1e_Elanco%20Cow%20Body_condition_scoring_V3.pdf)
+
+    [video](https://www.youtube.com/watch?v=BfW97OH02E0)
+
+    ## Milk production
+
+        Milk production refers to the process by which mammals, particularly cows in this context, produce milk.
+        
+        how to measure it :-
+        1. Milking Records:
+        - Daily Records: Keeping daily records of the amount of milk produced by each cow is a fundamental method. This can be done by recording the volume of milk collected during each milking session.
+        - Milking Frequency: The number of times a cow is milked each day can impact overall milk production. Most dairy cows are milked two or three times a day.
+
+        2. Milk Meters:
+            - Installing milk meters is a more automated approach. These devices are attached to milking machines and can accurately measure the amount of milk produced by each cow during each milking session.
+
+        3. Weighing:
+            - An alternative method is to weigh the milk. This can be done by weighing the milk container before and after milking. The weight difference provides an estimate of the milk produced.
+
+    ## Heart rate score
+
+        The adult cow has a heart rate of between 48 and 84 beats per minute.
+        
+        how to measure it :-
+        This can be assessed by using a stethoscope and listening over the left hand side of the cow's chest behind the cow's elbow.
+
+    ## Respiratory Rate
+
+        Respiratory rate is typically measured in breaths per minute (bpm). A normal respiratory rate for a healthy adult cow at rest is around 10 to 30 breaths per minute, but this can vary based on factors such as age, size, and environmental conditions.
+        how to measure it :-
+        This can be assessed by quietly watching the cow's ribs, count the number of times they move out on inspiration in 15 seconds and then multiply by 4.  A cow's respiratory rate may vary with the ambient temperature and if the cow is stressed but the adult cow's respiratory rate should be between 26 and 50 breaths per minute.
+
+    ## Eating Duration
+
+
+        The eating duration of a cow, or the time it spends eating, is an important aspect of its behavior and can provide insights into its health, nutrition, and overall well-being. Monitoring eating duration can be particularly relevant in dairy farming and beef production to ensure that cows are receiving adequate nutrition and are not facing any health issues.
+        how to measure it :-
+        This can be assessed by quietly watching the cow. count the time it take to eat and measure it using a stop watch.
+
+    ## Walking Capacity
+
+        The Walking Capacity is the cow's ability to walk or move around, and it is an important aspect of its overall well-being. A healthy walking capacity is indicative of good mobility, sound limb structure, and proper joint function. Regularly observe the cow's gait and movement patterns. A cow with a normal walking capacity will move freely and without obvious signs of lameness or discomfort.
+        how to measure it :-
+        1. Observe the cow's gait and movement patterns. Record it manually.
+
+        2. Use technology, such as motion sensors or accelerometers, to monitor the activity and movement patterns of cows. These tools can provide data on walking distances and times.
+
+    ## Lying down duration
+
+        The lying down duration of a cow refers to the amount of time a cow spends in a lying position. Monitoring lying down behavior is an essential aspect of assessing the well-being and comfort of cows. Cows typically lie down for various reasons, including rest, rumination, and sleep. Changes in lying down patterns can be indicative of health 
+        
+        how to measure it:-
+
+        1. Observe the cows to assess their lying down behavior. Record it manually.
+
+        2. Use sensors or monitoring systems to track the activity of cows, including lying down times. These sensors may be attached to collars or ear tags and can provide data on behavioral patterns.
+
+    ## Sleep duration
+
+        Cows, like many mammals, require periods of sleep for their well-being. Sleep duration for cows refers to the amount of time they spend in various sleep stages, which include both non-REM (Rapid Eye Movement) sleep and REM sleep. During sleep, cows experience relaxation, rest, and physiological processes important for their overall health. 
+        
+        how to measure it:-
+
+        1.  Observe the cows to determine their sleep patterns. Sleep in cows typically involves lying down, and they may exhibit different lying positions during sleep, including sternal (on their chest) or lateral (on their side) positions.. Record it manually.
+
+        2. Use sensors or monitoring systems to track the activity of cows, including sleeping times. These sensors may be attached to collars or ear tags and can provide data on behavioral patterns.
+
+    ## Ruminating Rate
+
+        After eating, cows engage in rumination, a process where they regurgitate and re-chew their food to aid in digestion. Monitoring the time spent on rumination, in addition to actual eating time, can provide a more comprehensive view of their digestive health. They will eat for 3-4 hours.  Normal cows will spend on average 6 hours chewing the cud each day (depending on their diet), most of this will be when they are lying.
+        how to measure it :-
+        1.  Observe the cows to determine their ruminating rate. ruminating in cows typically involves lying down. Record it manually.
+
+        2. Use sensors or monitoring systems to track the activity of cows, including ruminating rate. These sensors may be attached to collars or ear tags and can provide data on behavioral patterns.
+
+    ## Rumen Fill
+
+        The rumen in the cow is located predominantly on the left hand side of the abdomen and the area of it that can be detected is the top of the flank between the last rib and the pelvis; if a cow is off her feed the rumen will appear as a sharp triangle as it is empty, the more the rumen fills, the more this space fills.  If the space protrudes outwards, this may be a sign of bloat, seek veterinary advice.  There is a scoring system for rumen fill that vets may use, the rumen is assigned a score from 1 to 5.
+        how to measure it :-
+        1. Observation of a cow's left side (where the rumen is located) can provide an initial indication of rumen fill. A well-filled rumen appears rounded and firm. An empty or excessively distended rumen may be cause for concern.
+
+        To assess rumen fill, use the left-hand side of the cow behind the last rib, under the transverse processes of the spine and in front of the hook bone (Figure 1). It may be possible to feel a slightly firmer area which indicates the fibre mat of the rumen – sitting atop the liquid portion - sometimes with a small cap of gas on top (indicated by a softer area). The rumen fill score reflects intakes in the past 2–6 hours and targets are different depending on the physiological status of the cow (Figure 2).
+    """)
     # Creating two columns
     col1, col2 = st.columns(2)
 
